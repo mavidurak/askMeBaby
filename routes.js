0e9bce2aefdf1d37c0ffc3ca7a2fdeeb0bc0b311
Özgür Adem Işıklı <i.ozguradem@gmail.com>
2014-08-28 15:02:49 +0300

    Fix indents

--- routes.js
+++ routes.js
@@ -1,33 +1,33 @@
 // Route Configuration
 Router.configure({
-	layoutTemplate:'layout',
-  	load: function () {
-  		$('.navbar-menu-li').each(function() {
-  			$(this).removeClass('active');
-  		});
-		$("ul").find("[data-menu-key='" + this.route.name +"']")
-			   .parent()
-			   .addClass('active');
-  	}	
+    layoutTemplate:'layout',
+    load: function () {
+        $('.navbar-menu-li').each(function() {
+            $(this).removeClass('active');
+        });
+        $("ul").find("[data-menu-key='" + this.route.name +"']")
+               .parent()
+               .addClass('active');
+    }   
 });
 
 // Routes
 Router.map(function() {
-  this.route('home', {path: '/'});
-  this.route('login', {path: '/login'});
-  this.route('register', {path: '/register'});
-  this.route('profile', {path: '/profile'});
-  this.route('askQuestion', {path: '/question/ask'});
+    this.route('home', {path: '/'});
+    this.route('login', {path: '/login'});
+    this.route('register', {path: '/register'});
+    this.route('profile', {path: '/profile'});
+    this.route('askQuestion', {path: '/question/ask'});
 });
 
 // Navbar Menus For Titles
 if (Meteor.isClient) {
 
-	Template.layout.menus = [
-		{name: 'home', title: 'Home', link: ''},
-		{name: 'login', title: 'Login', link: 'login'},
-		{name: 'register', title: 'Register', link: 'register'},
-		{name: 'profile', title: 'Profile', link: 'profile'}
-	];
+    Template.layout.menus = [
+        {name: 'home', title: 'Home'},
+        {name: 'login', title: 'Login'},
+        {name: 'register', title: 'Register'},
+        {name: 'profile', title: 'Profile'}
+    ];
 
 }
